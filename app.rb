033cb7b62180e11ed391d8576325bfd0f491c769
Irene Njeru <irenenjeru8@gmail.com>
2022-06-25 00:48:08 +0300

    install sqlite3 gem

--- app.rb
+++ app.rb
@@ -1,19 +1,27 @@
 require 'rubygems'
 require 'sinatra'
 
-get '/:id' do
-    #get all todos of user_id = :id
+get '/' do
+    @res = "root page"
+end
+
+get '/:user_id' do
+    #get all todos of user_id = :user_id
     #table: todos table
+    @res = "method: get /id. route id is #{params[:user_id]}"
 end
 
-put '/:update/id' do
-    #update todo with todo_id = :id with data sent by user
+put '/update/:todo_id' do
+    #update todo with todo_id = :todo_id with data sent by user
+    @res = "method: put update/id. route id is #{params[:todo_id]}"
 end
 
-delete '/delete/:id' do
-    #delete todo with todo_id = :id
+delete '/delete/:todo_id' do
+    #delete todo with todo_id = :todo_id
+    @res = "method: delete delete/id. route id is #{params[:todo_id]}"
 end
 
 post '/create/' do
     #create a new todo with details sent by user
+    @res = "method: post create/"
 end
