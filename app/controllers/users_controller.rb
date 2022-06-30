6d836c8cea17794db86fc492930f2847a2ddc41c
Irene Njeru <irenenjeru8@gmail.com>
2022-06-30 14:03:10 +0300

    modify creaete method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
             user: params[:users],
             user: params[:user]
         )
-    new_user.to_json(include: [:todos])
+    new_user.to_json
     end
 
 end
